8df36bb13230a95f80842fd3415ac2f8079c9e47
sjpark538 <phi5388@gmail.com>
2022-06-25 12:20:31 -0400

    header

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.scss"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-container">
+                <div className="header-container--title">To Do List</div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
